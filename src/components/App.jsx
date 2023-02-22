3a0b1247e01ec359c99e4043c76bc1609cf32d9d
Xopar <xoparr@gmail.com>
2023-02-22 17:22:39 -0600

    Added a lot of item images to item_lookup and slightly reorganized the oot items. Fixed some typos in names and began testing primary layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,18 +10,22 @@ import { Item } from 'components/Item'
 // This component has code that runs every time it is rendered that only needs to run on the first time it is rendered.
 // I believe there is a hook that can be used to resolve this. So look into it
 
+let tempTracker = 0;
+
+// Take 3px margin out of app.css. Only there for easy comparison to existing layout
 
 // This element will be defined externally and selected via a dropdown or url parameter and passed to this componentent
-// const tempTrackerLayoutIdList = [
-//   "OOT_FOREST_MEDALLION", "OOT_FIRE_MEDALLION", "OOT_WATER_MEDALLION", "OOT_SHADOW_MEDALLION", "OOT_SPIRIT_MEDALLION", "OOT_LIGHT_MEDALLION",
-//   "OOT_PROGRESSIVE_SCALE", "BLANK", "BLANK", "BLANK", "BLANK", "OOT_BOTTLE_RUTOS",
-//   "OOT_SLINGSHOT", "OOT_BOMB_BAG", "OOT_BOOMERANG", "OOT_PROGRESSIVE_STRENGTH", "OOT_MAGIC_WLENS", "OOT_COMPOSITE_SPELLS",
-//   "OOT_PROGRESSIVE_HOOKSHOT", "OOT_BOW", "OOT_COMPOSITE_ARROWS", "OOT_HAMMER", "OOT_COMPOSITE_BOOTS", "OOT_MIRROR_SHIELD",
-//   "OOT_PROGRESSIVE_CHILD_TRADE", "OOT_OCARINA", "OOT_BEANS", "OOT_KOKIRI_SWORD", "OOT_GERUDO_CARD", "OOT_COMPOSITE_TUNICS",
-//   "OOT_ZELDAS_LULLABY_WCHECK", "OOT_EPONAS_SONG_WCHECK", "OOT_SARIAS_SONG_WCHECK", "OOT_SUNS_SONG_WCHECK", "OOT_SONG_OF_TIME_WCHECK", "OOT_SONG_OF_STORMS_WCHECK",
-//   "OOT_MINUET_WCHECK", "OOT_BOLERO_WCHECK", "OOT_SERENADE_WCHECK", "OOT_NOCTURNE_WCHECK", "OOT_REQUIEM_WCHECK", "OOT_PRELUDE_WCHECK"
-// ]
-const tempTrackerLayoutIdList = ["OOT_FOREST_MEDALLION", "MM_BOMB_BAG_WKEG", "OOT_SLINGSHOT", "OOT_COMPOSITE_TUNICS", "OOT_ZELDAS_LULLABY_WCHECK"]
+const tempTrackerLayoutIdList = [
+  "OOT_FOREST_MEDALLION", "OOT_FIRE_MEDALLION", "OOT_WATER_MEDALLION", "OOT_SHADOW_MEDALLION", "OOT_SPIRIT_MEDALLION", "OOT_LIGHT_MEDALLION",
+  "OOT_PROGRESSIVE_SCALE", "BLANK", "BLANK", "BLANK", "BLANK", "OOT_BOTTLE_RUTOS",
+  "OOT_SLINGSHOT", "OOT_BOMB_BAG", "OOT_BOOMERANG", "OOT_PROGRESSIVE_STRENGTH", "OOT_MAGIC_WLENS", "OOT_COMPOSITE_SPELLS",
+  "OOT_PROGRESSIVE_HOOKSHOT", "OOT_BOW", "OOT_COMPOSITE_ARROWS", "OOT_HAMMER", "OOT_COMPOSITE_BOOTS", "OOT_MIRROR_SHIELD",
+  "OOT_PROGRESSIVE_CHILD_TRADE", "OOT_OCARINA", "OOT_BEANS", "OOT_KOKIRI_SWORD", "OOT_GERUDO_CARD", "OOT_COMPOSITE_TUNICS",
+  "OOT_ZELDAS_LULLABY_WCHECK", "OOT_EPONAS_SONG_WCHECK", "OOT_SARIAS_SONG_WCHECK", "OOT_SUNS_SONG_WCHECK", "OOT_SONG_OF_TIME_WCHECK", "OOT_SONG_OF_STORMS_WCHECK",
+  "OOT_MINUET_WCHECK", "OOT_BOLERO_WCHECK", "OOT_SERENADE_WCHECK", "OOT_NOCTURNE_WCHECK", "OOT_REQUIEM_WCHECK", "OOT_PRELUDE_WCHECK"
+]
+
+// const tempTrackerLayoutIdList = ["OOT_FOREST_MEDALLION", "MM_BOMB_BAG_WKEG", "OOT_SLINGSHOT", "OOT_COMPOSITE_TUNICS", "OOT_ZELDAS_LULLABY_WCHECK"]
 
 const initializeTrackerState = (layout) => {
   const initialState = layout.reduce((tot, item) => {
@@ -49,6 +53,11 @@ export function Tracker() {
   }, []);
 
   const [trackerState, setTrackerState] = React.useState(initializeTrackerState(trackerLayout));
+  
+  
+  tempTracker += 1;
+  console.log(`Rendering #${tempTracker}`)
+
 
 
   const updateSingleItem = (pendingState) => {
