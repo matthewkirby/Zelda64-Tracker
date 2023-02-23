21698a0394664995856284e7cbd375abd1b2bfe7
Xopar <xoparr@gmail.com>
2023-02-22 20:00:40 -0600

    Typo causing itemInfo.name to be an array(1)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,14 +18,13 @@ let tempTracker = 0;
 // This element will be defined externally and selected via a dropdown or url parameter and passed to this componentent
 const tempTrackerLayoutIdList = [
   "OOT_FOREST_MEDALLION", "OOT_FIRE_MEDALLION", "OOT_WATER_MEDALLION", "OOT_SHADOW_MEDALLION", "OOT_SPIRIT_MEDALLION", "OOT_LIGHT_MEDALLION",
-  "OOT_PROGRESSIVE_SCALE", "BLANK", "BLANK", "BLANK", "BLANK", "OOT_BOTTLE_RUTOS",
+  "OOT_PROGRESSIVE_SCALE", "BLANK", { items:["OOT_KOKIRI_EMERALD", "OOT_GORON_RUBY", "OOT_ZORA_SAPPHIRE"], rows: 1, cols: 2}, "BLANK", "OOT_BOTTLE_RUTOS",
   "OOT_SLINGSHOT", "OOT_BOMB_BAG", "OOT_BOOMERANG", "OOT_PROGRESSIVE_STRENGTH", "OOT_MAGIC_WLENS", "OOT_COMPOSITE_SPELLS",
   "OOT_PROGRESSIVE_HOOKSHOT", "OOT_BOW", "OOT_COMPOSITE_ARROWS", "OOT_HAMMER", "OOT_COMPOSITE_BOOTS", "OOT_MIRROR_SHIELD",
   "OOT_PROGRESSIVE_CHILD_TRADE", "OOT_OCARINA", "OOT_BEANS", "OOT_KOKIRI_SWORD", "OOT_GERUDO_CARD", "OOT_COMPOSITE_TUNICS",
   "OOT_ZELDAS_LULLABY_WCHECK", "OOT_EPONAS_SONG_WCHECK", "OOT_SARIAS_SONG_WCHECK", "OOT_SUNS_SONG_WCHECK", "OOT_SONG_OF_TIME_WCHECK", "OOT_SONG_OF_STORMS_WCHECK",
   "OOT_MINUET_WCHECK", "OOT_BOLERO_WCHECK", "OOT_SERENADE_WCHECK", "OOT_NOCTURNE_WCHECK", "OOT_REQUIEM_WCHECK", "OOT_PRELUDE_WCHECK"
 ]
-
 
 // The square size will be another parent parameter, default is 50px
 const tempItemSize = null;
@@ -55,8 +54,10 @@ export function Tracker() {
   // Set up the tracker's item grid
   const trackerLayoutIdList = [ ...tempTrackerLayoutIdList ]; // This is a temp line. It will be a prop from a higher level comp with setup options.
   const trackerLayout = trackerLayoutIdList.reduce((tot, item) => {
-    return [ ...tot, {"name": [item], ...itemDict[item]} ]
+    return [ ...tot, {"name": item, ...itemDict[item]} ]
   }, []);
+
+  console.log(trackerLayout);
 
   // Build style object to pass to each item
   const itemSize = tempItemSize ?? 50; // Replace temp variable with prop passed from parent
