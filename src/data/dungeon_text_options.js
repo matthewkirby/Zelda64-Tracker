007833fe384fdda0ed8b3fe6ef5dd3e911e3e324
Xopar <xoparr@gmail.com>
2023-03-01 01:02:03 -0600

    Correct dungeon text options to always have free and unlabeled'

--- src/data/dungeon_text_options.js
+++ src/data/dungeon_text_options.js
@@ -1,26 +1,25 @@
-const OOT_DUNGEONS = ["FREE", "DEKU", "DCVN", "JABU",
-  "FRST", "FIRE", "WATR", "SHDW", "SPRT"];
-const OOT_DUNGEONS_LABELS = ["Free", "Deku Tree", "Dodongo's Cavern", "Jabu Jabu",
+const OOT_DUNGEONS = ["DEKU", "DCVN", "JABU", "FRST", "FIRE", "WATR", "SHDW", "SPRT"];
+const OOT_DUNGEONS_LABELS = ["Deku Tree", "Dodongo's Cavern", "Jabu Jabu",
   "Forest Temple", "Fire Temple", "Water Temple", "Shadow Temple", "Spirit Temple"];
 
 const MM_DUNGEONS = ["WOOD", "SNOW", "GBAY", "STWR"];
 const MM_DUNGEON_LABELS = ["Woodfall Temple", "Snowhead Temple", "Great Bay Temple",
   "Stone Tower Temple"];
 
-const UNKNOWN_IDENTIFIER = "";
-const UNKNOWN_LABEL = "Unknown";
+const BASE_IDENTIFIERS = ["", "FREE"];
+const BASE_LABELS = ["Unknown", "Free"];
 
 export const dungeonTextOptions = {
   "oot": {
-    identifiers: [ UNKNOWN_IDENTIFIER, ...OOT_DUNGEONS ],
-    labels: [ UNKNOWN_LABEL, ...OOT_DUNGEONS_LABELS ]
+    identifiers: [ ...BASE_IDENTIFIERS, ...OOT_DUNGEONS ],
+    labels: [ ...BASE_LABELS, ...OOT_DUNGEONS_LABELS ]
   },
   "mm": {
-    identifiers: [ UNKNOWN_IDENTIFIER, ...MM_DUNGEONS ],
-    labels: [ UNKNOWN_LABEL, ...MM_DUNGEON_LABELS]
+    identifiers: [ ...BASE_IDENTIFIERS, ...MM_DUNGEONS ],
+    labels: [ ...BASE_LABELS, ...MM_DUNGEON_LABELS]
   },
   "ootmm": {
-    identifiers: [ UNKNOWN_IDENTIFIER, ...OOT_DUNGEONS, ...MM_DUNGEONS ],
-    labels: [ UNKNOWN_LABEL, ...OOT_DUNGEONS_LABELS, ...MM_DUNGEON_LABELS ]
+    identifiers: [ ...BASE_IDENTIFIERS, ...OOT_DUNGEONS, ...MM_DUNGEONS ],
+    labels: [ ...BASE_LABELS, ...OOT_DUNGEONS_LABELS, ...MM_DUNGEON_LABELS ]
   }
 };
