2f54ea11a36100bfdd6ba3db3225695439704992
Xopar <xoparr@gmail.com>
2023-03-02 03:17:27 -0600

    Generalize Grid3x3 to hold from 2 to 9 items and span multiple rows and columns.

--- src/components/ItemTypes/Grid3x3.jsx
+++ src/components/ItemTypes/Grid3x3.jsx
@@ -1,16 +1,27 @@
 import { Item } from 'components/Item';
 
-const create2x2SubBoxStyles = (nItems, span, childTrackerOptions) => {
+const createSubBoxStyles = (nItems, span, childTrackerOptions, subGridSize) => {
   const units = childTrackerOptions.geometry.units;
   const oldWidth = childTrackerOptions.calc.itemSize.number;
   const gapSize = Math.min(childTrackerOptions.geometry.columnGap, childTrackerOptions.geometry.rowGap);
 
   // Resolve new sizes
   const totalSpace = span*oldWidth + (span-1)*gapSize;
-  const newWidth = totalSpace/2;
+  // const newWidth = span > 1 ? (totalSpace-gapSize)/subGridSize : totalSpace/subGridSize;
+  let newWidth = totalSpace/subGridSize;
+  if (newWidth-oldWidth > 0 && newWidth-oldWidth < 0.2*oldWidth) {
+    newWidth = oldWidth;
+  }
 
   // Set up new styles
-  const subBoxStyles = {}; 
+  const subBoxStyles = {
+    gridColumnEnd: `span ${span}`,
+    gridRowEnd: `span ${span}`,
+    justifyContent: nItems < 4 ? 'center' : 'space-between',
+    alignContent: nItems < 4 ? 'center' : 'space-between',
+    rowGap: (nItems < 4) ? childTrackerOptions.geometry.rowGap*(span-1) : 0,
+    columnGap: (nItems < 4) ? childTrackerOptions.geometry.columnGap*(span-1) : 0
+  }; 
   const newItemSize = { number: newWidth, style: { height: `${newWidth}${units}`, width: `${newWidth}${units}` } };
   const subElementStyles = [...Array(nItems).keys()].map((v) => {
     return { margin: (v === 0 && nItems < 4) ? `0 ${newWidth/2}${units}` : "" };
@@ -20,33 +31,39 @@ const create2x2SubBoxStyles = (nItems, span, childTrackerOptions) => {
   childTrackerOptions.itemSize = newWidth;
   childTrackerOptions.calc.itemSize = newItemSize;
 
-  return [ subBoxStyles, subElementStyles, childTrackerOptions ]
+  return [ subBoxStyles, subElementStyles, childTrackerOptions ];
 };
 
-// Until I have a better method, hard code positions and sizes given
-// the number of items and size of the grid3x3 item on the grid
 export const Grid3x3 = (props) => {
   const itemList = props.itemInfo.items;
   const nItems = itemList.length;
   const span = props.itemInfo.size;
 
+  // Check the number of items
   if (nItems < 2) {
     console.log("Grid3x3 elements should have at least 2 items.");
     return null;
-  } else if (nItems > 5) {
+  } else if (nItems > 9) {
     console.log("Grid3x3 elements currently only support less than 5 items");
     return null;
   }
 
-  if (span !== 1) {
-    console.log("Grid3x3 elements currently only support sizes of 1");
+  // Check the provided span
+  if (span < 1) {
+    console.log("Grid3x3 element span must be greater than 0.");
+    return null;
+  } else if (span > props.trackerOptions.geometry.nCols) {
+    console.log("Grid3x3 element span must be less than the number of tracker columns.");
     return null;
   }
 
+  // Build the subgrid styles (wrap this in a useEffect hook later?)
   const copyTrackerOptions = JSON.parse(JSON.stringify(props.trackerOptions));
   let subGridSettings = null;
   if (nItems < 5) {
-    subGridSettings = create2x2SubBoxStyles(nItems, span, copyTrackerOptions);
+    subGridSettings = createSubBoxStyles(nItems, span, copyTrackerOptions, 2);
+  } else if (nItems < 10) {
+    subGridSettings = createSubBoxStyles(nItems, span, copyTrackerOptions, 3);
   }
 
 
