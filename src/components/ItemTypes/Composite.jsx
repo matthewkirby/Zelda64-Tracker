8f950fa95aadc2080f1efc14e6beb992384dce0c
Xopar <xoparr@gmail.com>
2023-03-02 03:35:59 -0600

    Added legacy support for composite items.

--- src/components/ItemTypes/Composite.jsx
+++ src/components/ItemTypes/Composite.jsx
@@ -5,28 +5,41 @@
 const DEFAULT_STATE = false;
 
 export const Composite = ({ itemInfo, trackerState, trackerOptions, updateSingleItem, extraStyles }) => {
-
   const itemSizeStyle = trackerOptions.calc.itemSize.style;
-  const [leftItem, rightItem] = itemInfo.items;
+  const [leftItem, rightItem, legacyItem] = itemInfo.items;
   const leftState = trackerState[leftItem] ?? DEFAULT_STATE;
   const rightState = trackerState[rightItem] ?? DEFAULT_STATE;
   const itemState = [leftState, rightState];
+  const isLegacy = itemInfo.legacy;
 
   // Build the list of classes
-  const leftClassList = ["itm-base", leftItem];
+  const leftClassList = ["itm-base", isLegacy ? "single-item" : leftItem];
   const rightClassList = ["itm-base", rightItem];
-  if (!leftState && !rightState) {
-    leftClassList.push("left-half", "itm-false");
-    rightClassList.push("right-half", "itm-false");
-  } else if (leftState && !rightState) {
-    leftClassList.push("single-item");
-    rightClassList.push("itm-hidden");
-  } else if (!leftState && rightState) {
-    leftClassList.push("itm-hidden");
-    rightClassList.push("single-item")
+
+  if (isLegacy) {
+    if (!leftState && !rightState) {
+      leftClassList.push(legacyItem, "itm-false");
+    } else if (leftState && !rightState) {
+      leftClassList.push(leftItem, "single-item");
+    } else if (!leftState && rightState) {
+      leftClassList.push(rightItem, "single-item");
+    } else {
+      leftClassList.push(legacyItem);
+    }
   } else {
-    leftClassList.push("left-half");
-    rightClassList.push("right-half");
+    if (!leftState && !rightState) {
+      leftClassList.push("left-half", "itm-false");
+      rightClassList.push("right-half", "itm-false");
+    } else if (leftState && !rightState) {
+      leftClassList.push("single-item");
+      rightClassList.push("itm-hidden");
+    } else if (!leftState && rightState) {
+      leftClassList.push("itm-hidden");
+      rightClassList.push("single-item")
+    } else {
+      leftClassList.push("left-half");
+      rightClassList.push("right-half");
+    }
   }
 
   // Set up the button interaction
@@ -38,7 +51,7 @@ export const Composite = ({ itemInfo, trackerState, trackerOptions, updateSingle
   return (
     <div className='composite' style={{...itemSizeStyle, ...extraStyles}} onClick={() => onInteract(0)} onContextMenu={() => onInteract(1)} >
       <button className={leftClassList.join(" ")} />
-      <button className={rightClassList.join(" ")} />
+      {!isLegacy && <button className={rightClassList.join(" ")} />}
     </div>
   );
-}
+};
